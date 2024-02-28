ded5035839573ed956528580f5743c6aee5f49e8
creativekushwahaji <creativekushwahaji01@gmail.com>
2024-02-28 15:03:42 +0530

    done

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
     //connect to contract
     const dappazon = new ethers.Contract (
-      ' 0x5FbDB2315678afecb367f032d93F642f64180aa3',
+      '0x5FbDB2315678afecb367f032d93F642f64180aa3',
       ABI,
       provider
     )
